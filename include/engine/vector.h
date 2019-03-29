73c34c762a686c022da63a537a18fb251b507753
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-03-29 16:10:45 +0100

    Add Vec2i

---
+++ include/engine/vector.h
@@ -0,0 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2019 Nicolas Schneider
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef VECTOR_H
+#define VECTOR_H
+namespace dm
+{
+class Vec2i final
+{
+public:
+	int x;
+	int y;
+
+	Vec2i();
+	Vec2i(int x, int y);
+
+	~Vec2i() = default;
+
+	bool operator==(const Vec2i &rhs) const;
+	bool operator!=(const Vec2i &rhs) const;
+
+	Vec2i operator+(const Vec2i &rhs) const;
+	Vec2i& operator+=(const Vec2i &rhs);
+
+	Vec2i operator-(const Vec2i &rhs) const;
+	Vec2i& operator-=(const Vec2i &rhs);
+
+	Vec2i operator*(int rhs) const;
+	Vec2i& operator*=(int rhs);
+
+	Vec2i operator/(float rhs) const;
+	Vec2i& operator/=(float rhs);
+
+	float GetMagnitude() const;
+
+	static Vec2i Lerp(const Vec2i& v1, const Vec2i& v2, float t);
+	static float AngleBetween(const Vec2i& v1, const Vec2i& v2);
+	static float Dot(const Vec2i& v1, const Vec2i& v2);
+};
+}
+#endif VECTOR_H
---
+++ src/engine/vector.cpp
@@ -0,0 +1,117 @@
+/*
+MIT License
+
+Copyright (c) 2019 Nicolas Schneider
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <engine/vector.h>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+namespace dm
+{
+Vec2i::Vec2i() : x(0), y (0) {}
+
+Vec2i::Vec2i(const int x, const int y) : x(x), y(y) {}
+
+bool Vec2i::operator==(const Vec2i& rhs) const
+{
+	return x == rhs.x && y == rhs.y;
+}
+
+bool Vec2i::operator!=(const Vec2i& rhs) const
+{
+	return x != rhs.x || y != rhs.y;
+}
+
+Vec2i Vec2i::operator+(const Vec2i& rhs) const
+{
+	return Vec2i(x + rhs.x, y + rhs.y);
+}
+
+Vec2i& Vec2i::operator+=(const Vec2i& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+
+	return *this;
+}
+
+Vec2i Vec2i::operator-(const Vec2i& rhs) const
+{
+	return Vec2i(x - rhs.x, y - rhs.y);
+}
+
+Vec2i& Vec2i::operator-=(const Vec2i& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+
+	return *this;
+}
+
+Vec2i Vec2i::operator*(const int rhs) const
+{
+	return Vec2i(x * rhs, y * rhs);
+}
+
+Vec2i& Vec2i::operator*=(const int rhs)
+{
+	x *= rhs;
+	y *= rhs;
+
+	return *this;
+}
+Vec2i Vec2i::operator/(const float rhs) const
+{
+	return Vec2i(static_cast<int>(x / rhs), static_cast<int>(y / rhs));
+}
+
+Vec2i& Vec2i::operator/=(const float rhs)
+{
+	x = static_cast<int>(x / rhs);
+	y = static_cast<int>(y / rhs);
+
+	return *this;
+}
+
+float Vec2i::GetMagnitude() const
+{
+	return sqrtf(x * x + y * y);
+}
+
+Vec2i Vec2i::Lerp(const Vec2i& v1, const Vec2i& v2, float t)
+{
+	return v1 + (v2 - v1)*t;
+}
+
+float Vec2i::AngleBetween(const Vec2i& v1, const Vec2i& v2)
+{
+	const auto dot = Vec2i::Dot(v1, v2);
+	const float angle = acosf(dot) / M_PI * 180.0f;
+	return (dot < 0.0f ? -1.0f : 1.0f) * angle;
+}
+
+float Vec2i::Dot(const Vec2i& v1, const Vec2i& v2)
+{
+	return v1.x*v2.x + v1.y*v2.y;
+}
+}
