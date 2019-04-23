e3d4d85891a0e095c1ac94aef1c23342d2541213
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-04-23 12:06:43 +0200

    Add UniformBuffer

---
+++ src/graphics/uniform_buffer.cpp
@@ -0,0 +1,76 @@
+/*
+MIT License
+
+Copyright (c) 2019 Nicolas Schneider
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include<graphics/uniform_buffer.h>
+
+namespace dm
+{
+UniformBuffer::UniformBuffer(const VkDeviceSize& size, const void* data) :
+	Buffer(size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, data)
+{}
+
+void UniformBuffer::Update(const void* newData) const
+{
+	void *data;
+	Buffer::MapMemory(&data);
+	std::memcpy(data, newData, static_cast<std::size_t>(m_Size));
+	Buffer::UnmapMemory();
+}
+
+VkDescriptorSetLayoutBinding UniformBuffer::GetDescriptorSetLayout(const uint32_t& binding,
+	const VkDescriptorType& descriptorType, const VkShaderStageFlags& stage, const uint32_t& count)
+{
+	VkDescriptorSetLayoutBinding descriptorSetLayoutBinding = {};
+	descriptorSetLayoutBinding.binding = binding;
+	descriptorSetLayoutBinding.descriptorType = descriptorType;
+	descriptorSetLayoutBinding.descriptorCount = 1;
+	descriptorSetLayoutBinding.stageFlags = stage;
+	descriptorSetLayoutBinding.pImmutableSamplers = nullptr;
+	return descriptorSetLayoutBinding;
+}
+
+WriteDescriptorSet UniformBuffer::GetWriteDescriptor(const uint32_t& binding, const VkDescriptorType& descriptorType,
+	const std::optional<OffsetSize>& offsetSize) const
+{
+	VkDescriptorBufferInfo bufferInfo = {};
+	bufferInfo.buffer = m_Buffer;
+	bufferInfo.offset = 0;
+	bufferInfo.range = m_Size;
+
+	if (offsetSize)
+	{
+		bufferInfo.offset = offsetSize->GetOffset();
+		bufferInfo.range = offsetSize->GetSize();
+	}
+
+	VkWriteDescriptorSet descriptorWrite = {};
+	descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	descriptorWrite.dstSet = VK_NULL_HANDLE;
+	descriptorWrite.dstBinding = binding;
+	descriptorWrite.dstArrayElement = 0;
+	descriptorWrite.descriptorCount = 1;
+	descriptorWrite.descriptorType = descriptorType;
+	return WriteDescriptorSet(descriptorWrite, bufferInfo);
+}
+}
